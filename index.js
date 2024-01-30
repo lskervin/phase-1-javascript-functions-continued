48985003b8d32758772a9077ac5fbe60a5091512
LeightonSkervin <lskervin23@gmail.com>
2024-01-30 16:38:23 -0500

    lab complete

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function saturdayFun(activity = 'roller-skate') {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  console.log(saturdayFun('bathe my dog'));
+
+  const mondayWork = function(activity = 'go to the office') {
+    return `This Monday, I will ${activity}.`;
+  };
+  console.log(mondayWork());
+
+function wrapAdjective(symbol) {
+  return function(adjective) {
+    return `You are ${symbol}${adjective}${symbol}!`;
+  };
+}
